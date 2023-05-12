379dd709f2d97cd1c4a482f4b156f1afac44f45a
TekMath <dokuhebi.game@gmail.com>
2023-05-12 13:29:47 +0200

    📦 Added node & list

---
+++ lib/common/node/new.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** Node type new function
+*/
+
+#include <stdlib.h>
+#include "utils/malloc2.h"
+#include "node/defs.h"
+
+node_t *node_new(node_data_t data)
+{
+    node_t *node = malloc2(sizeof(node_t));
+
+    if (!node)
+        return NULL;
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
+}
