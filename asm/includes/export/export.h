dfa27ad487787cc7b8ee44f179f7db0bc8eb6fbb
Flavien Chenu <flavien.chenu@epitech.eu>
2023-05-16 13:52:10 +0200

    ✨ Added champion exportation function

---
+++ asm/includes/export/export.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2023
+** Corware
+** File description:
+** Exportation phase functions
+*/
+
+#pragma once
+
+#include <stdbool.h>
+#include "common/include/champion/defs.h"
+
+/**
+ * @brief Export given champion to binary file.
+ * @param filename Filename in which export given champion
+ * @param champion Champion to export
+ * @return Status of exporting success
+ */
+bool asm_export_champion(char *filename, champion_t *champion);
+
+/**
+ * @brief Create file used for exportation
+ * @param filename Filename to create for exportation
+ * @return File descriptor corresponding to created file or -1 on failure
+ **/
+int asm_export_create_file(char *filename);
