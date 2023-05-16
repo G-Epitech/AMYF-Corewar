e2cf26105d943c82364bb5129ba17e3058576507
Flavien Chenu <flavien.chenu@epitech.eu>
2023-05-16 22:38:16 +0200

    🧪 Added tests for exportation champion function

--- tests/criterion/asm/export/create_file.c
+++ tests/criterion/asm/export/create_file.c
@@ -6,22 +6,36 @@
 */
 
 #include <unistd.h>
+#include <sys/resource.h>
 #include <criterion/criterion.h>
 #include "export/defs.h"
 #include "export/export.h"
 
 Test(asm_export_create_file_tests, basic)
 {
-    int file = asm_export_create_file("champion1.core");
+    int file = asm_export_create_file("champion1.cor");
 
     cr_assert(file != ASM_EXPORT_NULL_FD);
     if (file != ASM_EXPORT_NULL_FD) {
         close(file);
-        remove("champion1.core");
+        remove("champion1.cor");
     }
 }
 
 Test(asm_export_create_file_tests, with_null_file_name)
 {
     cr_assert(asm_export_create_file(NULL) == ASM_EXPORT_NULL_FD);
+}
+
+Test(asm_export_create_file_tests, with_open_fail)
+{
+    struct rlimit save;
+    struct rlimit limit;
+
+    getrlimit(RLIMIT_NOFILE, &limit);
+    getrlimit(RLIMIT_NOFILE, &save);
+    limit.rlim_cur = 1;
+    setrlimit(RLIMIT_NOFILE, &limit);
+    cr_assert(asm_export_create_file("champion4.cor") == ASM_EXPORT_NULL_FD);
+    setrlimit(RLIMIT_NOFILE, &save);
 }
--- tests/criterion/asm/export/export.c
+++ tests/criterion/asm/export/export.c
@@ -17,15 +17,38 @@ Test(asm_export_tests, basic)
     int file = ASM_EXPORT_NULL_FD;
     champion_t *champion = champion_new();
 
-    cr_assert(asm_export_champion("champion2.core", champion));
-    file = open("champion2.core", O_WRONLY);
+    cr_assert(asm_export_champion("champion2.cor", champion));
+    file = open("champion2.cor", O_WRONLY);
     cr_assert(file != ASM_EXPORT_NULL_FD);
-    if (file != ASM_EXPORT_NULL_FD) {
-        
-    }
+    if (file != ASM_EXPORT_NULL_FD)
+        close(file);
+    champion_free(champion);
 }
 
-Test(asm_export_create_file_tests, with_null_file_name)
+Test(asm_export_tests, with_null_filename)
 {
-    cr_assert(asm_export_create_file(NULL) == ASM_EXPORT_NULL_FD);
+    champion_t *champion = champion_new();
+
+    cr_assert_not(asm_export_champion(NULL, champion));
+    champion_free(champion);
+}
+
+Test(asm_export_tests, with_null_champion)
+{
+    cr_assert_not(asm_export_champion("champion3.cor", NULL));
+}
+
+Test(asm_export_tests, with_open_fail)
+{
+    struct rlimit save;
+    struct rlimit limit;
+    champion_t *champion = champion_new();
+
+    getrlimit(RLIMIT_NOFILE, &limit);
+    getrlimit(RLIMIT_NOFILE, &save);
+    limit.rlim_cur = 1;
+    setrlimit(RLIMIT_NOFILE, &limit);
+    cr_assert_not(asm_export_champion("champion5.cor", champion));
+    setrlimit(RLIMIT_NOFILE, &save);
+    champion_free(champion);
 }
