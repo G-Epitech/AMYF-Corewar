446bd28e7b288a7e1e04e84f9abf9d724d250abf
Fradet <axel.fradet@epitech.eu>
2023-05-17 14:30:29 +0200

    🚚 Deplaced utils function in lib/my

--- asm/sources/parsing/utils/utils.c
+++ asm/sources/parsing/utils/utils.c
@@ -9,41 +9,9 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "my/includes/my.h"
 #include "parsing/utils.h"
+#include "my/includes/my.h"
 #include "common/includes/utils/malloc2.h"
-
-void free_str_tab(char **tab)
-{
-    int index = 0;
-
-    while (tab[index] != NULL) {
-        free(tab[index]);
-    }
-    free(tab);
-}
-
-char *strconcat(char *dest, char *src)
-{
-    int len_dest = my_strlen(dest);
-    int len_src = my_strlen(src);
-    char *new = malloc2(sizeof(char) * (len_dest + len_src + 1));
-    int index = 0;
-
-    if (!new)
-        return NULL;
-    while (dest[index] != '\0') {
-        new[index] = dest[index];
-        index++;
-    }
-    for (int index_src = 0; src[index_src] != '\0'; index_src++) {
-        new[index] = src[index_src];
-        index++;
-    }
-    new[len_dest + len_src] = '\0';
-    free(dest);
-    return new;
-}
 
 char *parsing_filter_comment(char *input_user)
 {
