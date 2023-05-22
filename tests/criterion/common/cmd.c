db6f93bfb95d3c2b4e2d2ee8782e024d4f07fcdc
TekMath <dokuhebi.game@gmail.com>
2023-05-22 15:48:34 +0200

    🐛 Fixed tests

--- tests/criterion/common/cmd.c
+++ tests/criterion/common/cmd.c
@@ -8,6 +8,7 @@
 #include <criterion/theories.h>
 #include <criterion/criterion.h>
 #include "cmd/cmd.h"
+#include "op/defs.h"
 #include "utils/malloc2.h"
 
 Test(commands_functions, new_command) {
@@ -16,7 +17,7 @@ Test(commands_functions, new_command) {
     cr_assert_not_null(cmd);
     cr_assert_null(cmd->label);
     for (size_t i = 0; i < MAX_ARGS_NUMBER; i++)
-        cr_assert_eq(cmd->parameters[i].type, P_NULL);
+        cr_assert_eq(cmd->parameters[i].type, T_NULL);
     cmd_free(cmd);
 }
 
@@ -31,10 +32,10 @@ Test(commands_functions, new_command_malloc_fail) {
 
 TheoryDataPoints(commands_functions, append_param) = {
     DataPoints(int, -100, -1, 0, 100, 10000),
-    DataPoints(type_parameter_t, P_NULL, P_REGISTER, P_DIRECT, P_INDIRECT)
+    DataPoints(char, T_NULL, T_REG, T_DIR, T_IND)
 };
 
-Theory((int value, type_parameter_t type), commands_functions, append_param) {
+Theory((int value, char type), commands_functions, append_param) {
     cmd_t *cmd = cmd_new();
 
     cr_assert(cmd_append_parameter(cmd, (parameter_t) {value, type}));
@@ -54,19 +55,19 @@ Test(commands_functions, append_param_multi) {
     cmd_t *cmd = cmd_new();
     bool status = true;
 
-    status = cmd_append_parameter(cmd, (parameter_t) {0, P_DIRECT});
+    status = cmd_append_parameter(cmd, (parameter_t) {0, T_DIR});
     cr_assert(status);
     cr_assert_eq(cmd->parameters[0].value, 0);
-    status = cmd_append_parameter(cmd, (parameter_t) {1, P_DIRECT});
+    status = cmd_append_parameter(cmd, (parameter_t) {1, T_DIR});
     cr_assert(status);
     cr_assert_eq(cmd->parameters[1].value, 1);
-    status = cmd_append_parameter(cmd, (parameter_t) {2, P_DIRECT});
+    status = cmd_append_parameter(cmd, (parameter_t) {2, T_DIR});
     cr_assert(status);
     cr_assert_eq(cmd->parameters[2].value, 2);
-    status = cmd_append_parameter(cmd, (parameter_t) {3, P_DIRECT});
+    status = cmd_append_parameter(cmd, (parameter_t) {3, T_DIR});
     cr_assert(status);
     cr_assert_eq(cmd->parameters[3].value, 3);
-    status = cmd_append_parameter(cmd, (parameter_t) {4, P_DIRECT});
+    status = cmd_append_parameter(cmd, (parameter_t) {4, T_DIR});
     cr_assert(!status);
     cmd_free(cmd);
 }
