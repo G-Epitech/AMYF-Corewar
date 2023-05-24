48bf832d461f4c80d95a20bc22bbf8ec5c2ed9f0
Flavien Chenu <flavien.chenu@epitech.eu>
2023-05-24 19:33:28 +0200

    🐛 Fixed bad parameters declaration in operations arrray

--- lib/common/sources/op.c
+++ lib/common/sources/op.c
@@ -21,12 +21,12 @@ const op_t op_tab[] = {
     {"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG},
     8, 6, true},
     {"zjmp", 1, {T_IND}, 9, 20, false},
-    {"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25, true},
+    {"ldi", 3, {T_REG | T_IND, T_IND | T_REG, T_REG}, 10, 25, true},
     {"sti", 3, {T_REG, T_REG | T_IND, T_IND | T_REG}, 11, 25, true},
     {"fork", 1, {T_IND}, 12, 800, false},
     {"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, true},
     {"lldi", 3, {T_REG | T_IND, T_IND | T_REG, T_REG}, 14, 50, true},
-    {"lfork", 1, {T_DIR}, 15, 1000, false},
+    {"lfork", 1, {T_IND}, 15, 1000, false},
     {"aff", 1, {T_REG}, 16, 2, true},
     {0, 0, {0}, 0, 0, false}
 };
