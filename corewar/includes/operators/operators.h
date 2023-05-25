cec8d339885c5ca571c0b962aea88832a0331698
TekMath <dokuhebi.game@gmail.com>
2023-05-25 13:46:49 +0200

    📝 Added new operators declaration

--- corewar/includes/operators/operators.h
+++ corewar/includes/operators/operators.h
@@ -56,10 +56,30 @@ parameter_t params[MAX_ARGS_NUMBER]);
 bool corewar_operators_sub(arena_t *arena, champion_fighter_t *champion,
 parameter_t params[MAX_ARGS_NUMBER]);
 
+/**
+ * @brief Operator of corewar (zjump).
+ * @param champion Champion that use this operator
+ * @param command Specific command structure of the operator
+ * @return true if the function success, false otherwise
+ */
+bool corewar_operators_zjump(arena_t *arena, champion_fighter_t *champion,
+parameter_t params[MAX_ARGS_NUMBER]);
+
+/**
+ * @brief Operator of corewar (ldi).
+ * @param champion Champion that use this operator
+ * @param command Specific command structure of the operator
+ * @return true if the function success, false otherwise
+ */
+bool corewar_operators_ldi(arena_t *arena, champion_fighter_t *champion,
+parameter_t params[MAX_ARGS_NUMBER]);
+
 static const operator_t operators_tab[] = {
     &corewar_operators_live,
     &corewar_operators_ld,
     &corewar_operators_st,
-    &corewar_operators_ld
+    &corewar_operators_ld,
+    &corewar_operators_zjump,
+    &corewar_operators_ldi,
     NULL
 };
