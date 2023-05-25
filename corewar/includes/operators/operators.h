b0092d4f086ec265546eb1a328427dfc652eeae4
Flavien Chenu <flavien.chenu@epitech.eu>
2023-05-25 14:31:54 +0200

    ✨Added and operation

--- corewar/includes/operators/operators.h
+++ corewar/includes/operators/operators.h
@@ -74,11 +74,22 @@ parameter_t params[MAX_ARGS_NUMBER]);
 bool corewar_operators_ldi(arena_t *arena, champion_fighter_t *champion,
 parameter_t params[MAX_ARGS_NUMBER]);
 
+/**
+ * @brief Operator of corewar (and).
+ * @param champion Champion that use this operator
+ * @param command Specific command structure of the operator
+ * @return true if the function success, false otherwise
+ */
+bool corewar_operators_and(arena_t *arena, champion_fighter_t *champion,
+parameter_t params[MAX_ARGS_NUMBER]);
+
 static const operator_t operators_tab[] = {
     &corewar_operators_live,
     &corewar_operators_ld,
     &corewar_operators_st,
-    &corewar_operators_ld,
+    &corewar_operators_add,
+    &corewar_operators_sub,
+    &corewar_operators_and,
     &corewar_operators_zjump,
     &corewar_operators_ldi,
     NULL
