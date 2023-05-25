81e5f5e557b190df493b7d5f3456f967c505f642
Flavien Chenu <flavien.chenu@epitech.eu>
2023-05-25 12:37:43 +0200

    📑 Updated st operation with register number check

--- corewar/sources/operators/st.c
+++ corewar/sources/operators/st.c
@@ -13,13 +13,15 @@ bool corewar_operators_st(arena_t *arena, champion_fighter_t *champion,
 parameter_t params[MAX_ARGS_NUMBER])
 {
     int reg = params[0].value - 1;
-    int value = 0;
+    int value = params[1].value;
     short addr = 0;
 
     if (reg < 0 || reg >= REG_NUMBER)
         return false;
     if (IS_T_REG(params[1].type)) {
-        value = champion->registers[params[1].value];
+        if (value < 0 || value >= REG_NUMBER)
+            return false;
+        value = champion->registers[value];
     } else {
         addr = arena_get_real_addr(champion->pc + params[1].value % IDX_MOD);
         value = arena->array[addr];
